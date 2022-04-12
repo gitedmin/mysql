fa0257d3c8c5768d13d920521066a5dd52de6ad2
EDWIN K J <01579@geojit.com>
2022-04-12 11:24:49 +0530

    Modified username and mysql path

--- run-mysql.bat
+++ run-mysql.bat
@@ -1,5 +1,7 @@
 @echo off
 title MySQL
 echo "Enter DB password"
-"C:\Program Files (x86)\MySQL\MySQL Server 5.5\bin\mysql.exe" -u quickfix_ncdex -p < clicmd.bat
+"PATH\mysql.exe" -u DBUSERNAME -p < clicmd.bat
+#call clicmd.bat next
 pause
+
